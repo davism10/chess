ee281e1375a0d1818005b2c17f11713bc430ac1b
davism10 <mckayladmg@gmail.com>
2025-02-25 09:59:09 -0700

    Adding all user clear service methods

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,5 +1,35 @@
 package service;
 
+import Model.AuthData;
+import Model.GameData;
+import Model.ListGamesResult;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import exception.ResponseException;
+
+import java.util.Collection;
+
 public class ClearService {
-    public void clear() {}
+    final private GameDAO gameMemory;
+    final private AuthDAO authMemory;
+    final private UserDAO userMemory;
+
+    public ClearService(GameDAO gameMemory, AuthDAO authMemory, UserDAO userMemory) {
+        this.gameMemory = gameMemory;
+        this.authMemory = authMemory;
+        this.userMemory = userMemory;
+    }
+
+    public void clear() throws ResponseException {
+        try {
+            gameMemory.clearAll();
+            authMemory.deleteAllAuth();
+            userMemory.clearAll();
+        }
+        catch (DataAccessException e) {
+            throw new ResponseException(500, "Error: " + e);
+        }
+    }
 }
